60ff8e1660c4c572f16382d016594b95c59169f0
ygz113404 <yagizomeraltun@windowslive.com>
2023-03-02 08:18:52 +0300

    Update Skills.js

--- src/Sections/Skills.js
+++ src/Sections/Skills.js
@@ -1,7 +1,15 @@
 function Skills() {
-    return (<>
-    Skills
-    </>  );
+    return (
+    <div id="skills" className="bg-black">
+    <h1 className="text-white text-3xl font-bold md:text-5xl text-center pt-6">My Skills</h1>
+    <div className="max-w-[1000px]  mx-auto grid md:grid-cols-4 gap-5   h-[800px] bg-black">
+     <div className="border h-[400px] my-[100px]">Javascript</div>
+     <div  className="border h-[400px] my-[100px]">React</div>
+     <div  className="border h-[400px] my-[100px]">HTML</div>
+     <div  className="border h-[400px] my-[100px]">JAVA</div>
+    </div>
+    </div>
+      );
 }
 
 export default Skills;
